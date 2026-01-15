684da5265f1453c30343cf45d3b61c8495814537
tcorrea <teofilocorrea@gmail.com>
2026-01-15 16:18:59 -0400

    feat: crear esquema en DuckDB para organizar la estructura de tablas
    - Se creó el esquema en DuckDB destinado a almacenar y organizar las tablas del proyecto.
    - Define la estructura organizacional para la capa de datos dentro del motor local.
    - Facilita la separación lógica y el orden del modelo durante la ingesta y el EDA.
    - Prepara el entorno para la gestión estructurada de tablas Bronze (RAW).

---
+++ sql/create_schema_table/create_schema.sql
@@ -0,0 +1,6 @@
+-- ========================================
+-- TEMPLATE: RAW_CREATE_SCHEMA
+-- Descripción: Crear esquema base (profiling)
+-- ========================================
+
+CREATE SCHEMA IF NOT EXISTS profiling;
